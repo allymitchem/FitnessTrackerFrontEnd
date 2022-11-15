e094484663402b1718d0b1e47fee2944697bacf6
Allyson Mitchem <allysonmitchem@gmail.com>
2022-11-14 21:54:52 -0700

    routines displaying, along with activities attached to routine

--- src/components/Routines.jsx
+++ src/components/Routines.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getRoutines } from "../api";
+
 const Routines = () => {
   const [routines, setRoutines] = useState([]);
   useEffect(() => {
@@ -17,14 +18,39 @@ const Routines = () => {
       </div>
       <div>
         {routines.map((routine) => {
+          console.log(routine, "I am routineeee");
           return (
-            <div id="routines" key={`routines-id${routine.id}`}>
-              <div>Creator Name:{routine.creatorName}</div>
-              <div>Name:{routine.name}</div>
-              <div>Goal:{routine.goal}</div>
-			  <div>Duration:{routine.duration}</div>
-			  <div>Count:{routine.activities.id}</div>
-			  <div>Description:{routine.description}</div>
+            <div className="routineActs">
+              <div id="routines" key={`routines-id${routine.id}`}>
+                <div>
+                  <b>Creator Name:</b> {routine.creatorName}
+                </div>
+                <div>
+                  <b>Routine Name:</b> {routine.name}
+                </div>
+                <div>
+                  <b>Goal:</b> {routine.goal}
+                </div>
+              </div>
+              {routine.activities.map((activity) => {
+                console.log(activity, "I am activityyy ");
+                return (
+                  <div id="activities">
+                    <div>
+                      <b>Activity Name:</b> {activity.name}
+                    </div>
+                    <div>
+                      <b>Description:</b> {activity.description}
+                    </div>
+                    <div>
+                      <b>Count:</b> {activity.count}
+                    </div>
+                    <div>
+                      <b>Duration:</b> {activity.duration}{" "}
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           );
         })}
@@ -34,5 +60,3 @@ const Routines = () => {
 };
 
 export default Routines;
-
-// {"id":580,"creatorId":282,"isPublic":true,"name":"play roulette","goal":"throw your life savings on one single spin of roulette","creatorName":"derekMiller","activities":[{"id":1,"name":"Not wide grip","description":"Dont do it","duration":1,"count":1,"routineActivityId":9494,"routineId":580}
