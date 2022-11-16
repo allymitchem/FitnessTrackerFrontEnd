e6fa962fd42750833ea8ff34352b96219c95fca2
Allyson Mitchem <allysonmitchem@gmail.com>
2022-11-15 17:31:00 -0700

    login is now working with useState

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,10 +3,10 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { loginUser } from "../api";
 
 const Login = (props) => {
-  // const [formData, setFormData] =useState({
-  // 	username: "",
-  // 	password:""
-  // })
+  const [formData, setFormData] =useState({
+  	username: "",
+  	password:""
+  })
   const [username, setUsername] = useState([]);
   // const [password, setPassword] = useState("")
   const navigate = useNavigate();
@@ -17,8 +17,8 @@ const Login = (props) => {
   async function handleSubmit(e) {
     e.preventDefault();
     // console.log(formData)
-    const username = e.target[0].value;
-    const password = e.target[1].value;
+    const username = formData.username;
+    const password = formData.password;
 
     const loggingInUser = await loginUser(username, password);
     const token = loggingInUser.token;
@@ -42,12 +42,12 @@ const Login = (props) => {
     <div>
       <h2>Please sign in</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="username" className="username" />
-        {/* // onChange={(e)=> setFormData({...formData, username:e.target.value})}
-		//  value={formData.username}/> */}
-        <input type="password" placeholder="password" className="username" />
-        {/* onChange={(e)=> setFormData({...formData, password:e.target.value})}
-		 value={formData.password}/> */}
+        <input type="text" placeholder="username" className="username" 
+         onChange={(e)=> setFormData({...formData, username:e.target.value})}
+		 value={formData.username}/>
+        <input type="password" placeholder="password" className="username" 
+        onChange={(e)=> setFormData({...formData, password:e.target.value})}
+		 value={formData.password}/>
         <button type="submit">Submit</button>
       </form>
       <NavLink to="register">Don't have an account? Sign up here</NavLink>
