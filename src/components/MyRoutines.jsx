9dbbf55164d9a786d51ed289f62e0019a08ce398
Allyson Mitchem <allysonmitchem@gmail.com>
2022-11-17 07:54:39 -0700

    my routines are displaying

--- src/components/MyRoutines.jsx
+++ src/components/MyRoutines.jsx
@@ -4,21 +4,45 @@ import CreateRoutine from "./CreateRoutine";
 import Routines from "./Routines";
 
 const MyRoutines = (props) => {
-  const username = props.username;
+//   const username = props.username;
+  const user= localStorage.getItem("user")
+  console.log(user, " this is user")
+  
   const [myRoutines, setMyRoutines] = useState([]);
   useEffect(() => {
     async function fetchMyRoutines() {
 		const token = localStorage.getItem('token')
-      if (username && token) {
-        const allMyRoutines = await getMyRoutine(username, token);
+      if (user && token) {
+       
+        const allMyRoutines = await getMyRoutine(user, token);
+        console.log(allMyRoutines, "this is all my routines")
         setMyRoutines(allMyRoutines);
+    } 
+        
       }
-    }
     fetchMyRoutines();
-  }, [username]);
+  }, [user]);
+
+  
   return (
     <div>
-		<CreateRoutine username={username}/>
+		<CreateRoutine user={user}/>
+        <h4>my routines under here</h4>
+        <div>
+            
+            {myRoutines.map((routine)=>{
+               console.log(myRoutines)
+                    return(
+                    <div>
+                        <div><b>
+                        Routine Name:</b> {routine.name}
+                        </div>
+                        <div><b>Goal:</b> {routine.goal} </div>
+                    </div>
+                    )
+                
+            })}
+        </div>
 		</div>
 			)}
             {/* <h4>myroutineshere</h4>
