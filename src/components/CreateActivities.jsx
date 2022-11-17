f89d0bc6bbbda33db977f78bd5ab4a6129798f1b
Allyson Mitchem <allysonmitchem@gmail.com>
2022-11-17 08:20:02 -0700

    added CreateActivities component

---
+++ src/components/CreateActivities.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const CreateActivities = ()=>{
+    return(
+        <h3> create activity here</h3>
+    )
+}
+
+
+export default CreateActivities
--- src/components/MyRoutines.jsx
+++ src/components/MyRoutines.jsx
@@ -4,52 +4,55 @@ import CreateRoutine from "./CreateRoutine";
 import Routines from "./Routines";
 
 const MyRoutines = (props) => {
-//   const username = props.username;
-  const user= localStorage.getItem("user")
-  console.log(user, " this is user")
-  
+  //   const username = props.username;
+  const user = localStorage.getItem("user");
+  console.log(user, " this is user");
+
   const [myRoutines, setMyRoutines] = useState([]);
   useEffect(() => {
     async function fetchMyRoutines() {
-		const token = localStorage.getItem('token')
+      const token = localStorage.getItem("token");
       if (user && token) {
-       
         const allMyRoutines = await getMyRoutine(user, token);
-        console.log(allMyRoutines, "this is all my routines")
+        console.log(allMyRoutines, "this is all my routines");
         setMyRoutines(allMyRoutines);
-    } 
-        
       }
+    }
     fetchMyRoutines();
   }, [user]);
 
-  
   return (
     <div>
-		<CreateRoutine user={user}/>
-        <h4>my routines under here</h4>
-        <div>
-            
-            {myRoutines.map((routine)=>{
-               console.log(myRoutines)
-                    return(
-                    <div>
-                        <div><b>
-                        Routine Name:</b> {routine.name}
-                        </div>
-                        <div><b>Goal:</b> {routine.goal} </div>
-                    </div>
-                    )
-                
-            })}
-        </div>
-		</div>
-			)}
-            {/* <h4>myroutineshere</h4>
-    //   {routines.map((routine) => { */}
-    {/* //     return( 
-	// 		// if (routine.creatorName === username){ */}
-	/* // 		// 	return(
+      <CreateRoutine user={user} />
+      <h4>my routines under here</h4>
+      <div>
+        {myRoutines.map((routine) => {
+          console.log(myRoutines);
+          return (
+            <div className="myRoutines" key ={`myRoutine-id${routine.id}`}>
+              <div>
+                <b>Routine Name:</b> {routine.name}
+              </div>
+              <div>
+                <b>Goal:</b> {routine.goal}{" "}
+              </div>
+              //will need to map over activities once activities are added
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+{
+  /* <h4>myroutineshere</h4>
+    //   {routines.map((routine) => { */
+}
+{
+  /* //     return( 
+	// 		// if (routine.creatorName === username){ */
+}
+/* // 		// 	return(
 
 	// 		// 	)
 	// 		// }
